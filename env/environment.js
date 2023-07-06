2683872d04cc9cb704982befc35b0c88225bbe37
FEDERICO CURRAO - GQue - DEVELOPER <68132577+fcurrao@users.noreply.github.com>
2023-07-06 18:08:10 -0300

    Add files via upload

--- env/environment.js
+++ env/environment.js
@@ -2,13 +2,12 @@
 // import env from './.env/env'
 
 export const environment = {
-    production: false,
-    prueba : "probandoooo",
-    mongoConnection : "mongodb+srv://fcurrao1:fcurrao1@cluster0.pzrcxla.mongodb.net/ecommerce?retryWrites=true&w=majority",
-    // baseUrl: 'https://localhost:60781/'
-    baseUrl: 'http://evim-api.dev.megatrans.com.ar/',
-    AUTH_CLIENT_ID_GITHUB: 'Iv1.7b358415b5d0e860'
-    
-  };
-  
-  
+  production: false,
+  prueba: "probandoooo",
+  mongoConnection:
+    "mongodb+srv://fcurrao1:fcurrao1@cluster0.pzrcxla.mongodb.net/ecommerce?retryWrites=true&w=majority",
+  // baseUrl: 'https://localhost:60781/'
+  baseUrl: "http://evim-api.dev.megatrans.com.ar/",
+  AUTH_CLIENT_ID_GITHUB: "Iv1.7b358415b5d0e860",
+  AUTH_CLIENT_SECRET_GITHUB: "178874e9f61399ecdb0e8606c9b60ed836f3b297",
+};
